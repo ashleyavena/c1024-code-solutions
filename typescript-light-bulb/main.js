bb33e3aab1ff7a7fd6d7edb4ba0894f64321ec33
Ashley Avena <agshelter7@gmail.com>
2024-11-06 22:56:19 +0000

    Completed typescript-light-bulb

--- typescript-light-bulb/main.js
+++ typescript-light-bulb/main.js
@@ -1,17 +1,14 @@
-"use strict";
-const $circleLight = document.querySelector('.circle-light ');
-if (!$circleLight)
-    throw new Error('$circleLight not found');
-const $circleDark = document.querySelector('.circle-dark ');
-if (!$circleDark)
-    throw new Error('$circleDark not found');
-const $dialog = document.querySelector('dialog');
-if (!$dialog)
-    throw new Error('$dialog not found');
-// const $containerTransition= document.querySelector('.container-transition')
-// if (!$containerTransition) throw new Error('$containerTransition not found');
+'use strict';
+const $circleLight = document.querySelector('.circle-light');
+if (!$circleLight) throw new Error('$circleLight not found');
+const $container = document.querySelector('.container');
+if (!$container) throw new Error('$container not found');
 $circleLight.addEventListener('click', () => {
-    $dialog.show();
-}, $circleDark.addEventListener('click', () => {
-    $dialog.close();
-}));
+  if ($circleLight.className === 'circle-light') {
+    $circleLight.className = 'circle-dark';
+    $container.className = 'container darkGround';
+  } else {
+    $circleLight.className = 'circle-light';
+    $container.className = 'container';
+  }
+});
