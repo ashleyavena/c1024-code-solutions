893328ea78c7aa428a3a85573427d62048e096c6
Ashley Avena <agshelter7@gmail.com>
2024-11-21 22:26:43 +0000

    completed typescript-async-await

--- typescript-async-await/exceptions.ts
+++ typescript-async-await/exceptions.ts
@@ -6,25 +6,19 @@ const startTime = Date.now();
 const elapsed = (): string =>
   `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
-// ~~~~~~~~~~~~~~~
-
 // function throwOnce(): Promise<void> {
 //   return read('foo', false)
 //     .then((msg) => console.log(elapsed(), 'throwOnce:', msg))
 //     .catch((error) => console.log(elapsed(), 'throwOnce Error:', error));
 // }
-
 async function throwOnce(): Promise<void> {
   try {
-    const message = await read('foo', false);
+    const message = await read('foo', true);
     console.log(elapsed(), 'throwOnce:', message);
   } catch (error) {
     console.log(elapsed(), 'throwOnce Error:', error);
   }
 }
-
-// ~~~~~~~~~~~~~~~
-
 // function throwSeveral(): Promise<void> {
 //   return read('foo1', false)
 //     .then((msg) => {
@@ -38,22 +32,18 @@ async function throwOnce(): Promise<void> {
 //     .then((msg) => console.log(elapsed(), 'throwSeveral3:', msg))
 //     .catch((error) => console.log(elapsed(), 'throwSeveral Error:', error));
 // }
-
 async function throwSeveral(): Promise<void> {
   try {
-    const message = await read('foo1', false);
+    const message = await read('foo1', true);
     console.log(elapsed(), 'throwSeveral1:', message);
-    const message2 = await read('foo2', false);
+    const message2 = await read('foo2', true);
     console.log(elapsed(), 'throwSeveral2:', message2);
-    const message3 = await read('foo3', false);
+    const message3 = await read('foo3', true);
     console.log(elapsed(), 'throwSeveral3:', message3);
   } catch (error) {
     console.log(elapsed(), 'throwSeveral Error:', error);
   }
 }
-
-// ~~~~~~~~~~~~~~~
-
 // function throwChained(): Promise<void> {
 //   return read('foo-chain', false)
 //     .then((msg1) => {
@@ -67,26 +57,24 @@ async function throwSeveral(): Promise<void> {
 //     .then((msg3) => console.log(elapsed(), 'throwChained3:', msg3))
 //     .catch((error) => console.log(elapsed(), 'throwChained Error:', error));
 // }
-
 async function throwChained(): Promise<void> {
   try {
-    const message1 = await read('foo-chain', false);
+    const message1 = await read('foo-chain', true);
     console.log(elapsed(), 'throwChained1:', message1);
-    const message2 = await read(message1, false);
+    const message2 = await read(message1, true);
     console.log(elapsed(), 'throwChained2:', message2);
-    const message3 = await read(message2, false);
+    const message3 = await read(message2, true);
     console.log(elapsed(), 'throwChained3:', message3);
   } catch (error) {
     console.log(elapsed(), 'throwChained Error:', error);
   }
 }
-
 // throwOnce()
 //   .then(() => throwSeveral())
 //   .then(() => throwChained());
 
-await throwOnce();
-await throwSeveral();
-await throwChained();
-
-// ~~~~~~~~~~~~~~~
+(async (): Promise<void> => {
+  await throwOnce();
+  await throwSeveral();
+  await throwChained();
+})();
