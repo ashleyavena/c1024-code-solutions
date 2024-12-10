05018273413edc1f0d0530ef780de682498ae56b
Ashley Avena <agshelter7@gmail.com>
2024-12-10 06:57:49 +0000

    completed react-props

---
+++ react-props/my-app/src/App.tsx
@@ -0,0 +1,31 @@
+import { Header } from './Header';
+import { Image } from './Image';
+import { Caption } from './Caption';
+import { Description } from './Description';
+import { Button } from './Button';
+import './App.css';
+
+const imageVar = '/starry-sky.jpeg';
+const capVar = 'A beautiful Image of Space';
+const descriptionVar = `  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt
+      sit dolorum accusantium sapiente reiciendis maxime dolores ullam delectus,
+      commodi placeat eveniet, quam voluptates facere et magnam architecto
+      quaerat velit voluptas rerum quos asperiores quis in! Nemo aut aliquam
+      quas dolore quaerat, error numquam odio modi eos vero. Modi nisi eos
+      autem? Iusto hic impedit aspernatur labore quos nesciunt? Ad dolore
+      pariatur aperiam nemo, maiores laboriosam quam quod nisi ipsum, dicta sunt
+      laborum similique repellat?`;
+
+const buttonVar = 'Click For Next Image';
+
+export default function App() {
+  return (
+    <>
+      <Header text="React Image Bank" />
+      <Image src={imageVar} />
+      <Caption text={capVar} />
+      <Description text={descriptionVar} />
+      <Button text={buttonVar} />
+    </>
+  );
+}
---
+++ react-props/react-props-and-expressions-quiz-notes.md
@@ -0,0 +1,47 @@
+# react-props-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What are props in React?
+  the means of passing arguments to React components when they are called;
+  used to pass data from one component to another, read-only and cannot be modified by the receiving component
+
+- How do you declare and access props in a component?
+  {} are used to access the values of the props object and use them in JSX
+  include the props as a parameter in a functional component or using this.props in a class component
+
+- How do you pass props to a component?
+  by adding attributes when the component is used, with the attribute names matching the expected prop names
+
+- How do you write JavaScript expressions in JSX?
+  Any valid JavaScript expression can be put inside the {}.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
