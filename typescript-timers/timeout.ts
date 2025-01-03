7ff10b6e30388f41a15ca5f460552bf5546d2deb
Ashley Avena <agshelter7@gmail.com>
2025-01-03 01:25:27 +0000

    fixes

--- typescript-timers/timeout.ts
+++ typescript-timers/timeout.ts
@@ -1,11 +1,9 @@
 const $h1 = document.querySelector('h1') as HTMLHeadElement;
 if (!$h1) throw new Error('not found');
 
-function messageChange(): any {
+function messageChange(): void {
   $h1.textContent = 'hello there';
   console.log(' text changed to: hello there');
 }
 
-setTimeout((): void => {
-  messageChange();
-}, 2000);
+setTimeout(messageChange, 2000);
