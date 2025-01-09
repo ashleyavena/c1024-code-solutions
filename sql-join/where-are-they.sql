192e6391609b29db223223ec409a2b03fb4d513b
Ashley Avena <agshelter7@gmail.com>
2025-01-09 23:18:28 +0000

    update

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -10,7 +10,17 @@ inner join
 inner join
 "countries" on "cities"."countryId" = "countries"."countryId";
 
-
+-- Select
+-- "addresses"."line1",
+-- "addresses"."district",
+-- "cities"."name" as "cityName",
+-- "countries"."name" as "countryName"
+-- from
+-- "addresses"
+-- join
+-- "cities" using ("cityId")
+--  join
+-- "countries" using ("countryId")
 
 -- Generate a list of all addresses in the system that includes
 -- their "line1", "cities"."name", and "district".
